9115f3d3f32a9f1e78467135b1b98dfb7f2f3232
Phatza <b.haphuoc@gmail.com>
2017-01-04 15:50:58 +0100

    Finalisation vueJS Todolist

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -7,21 +7,24 @@
       <div class="main">
         <input type="checkbox" class="toggle-all" v-model="allDone">
           <ul class="todo-list">
-              <li class="todo" v-for="todo in filteredTodos" :class="{completed: todo.completed}">
+              <li class="todo" v-for="todo in filteredTodos" :class="{completed: todo.completed, editing: todo === editing}">
                   <div class="view">
                     <input type="checkbox" class="toggle" v-model="todo.completed">
-                      <label>{{todo.name}}</label>
+                      <label @dblclick="editTodo(todo)">{{todo.name}}</label>
+                      <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
                   </div>
+                  <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus="todo === editing" @blur="doneEdit">
               </li>
           </ul>
       </div>
-      <footer class="footer">
+      <footer class="footer" v-show="hasTodos">
         <span class="todo-count"><strong>{{ remaining }}</strong> taches à faire</span>
         <ul class="filters">
           <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
           <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
           <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
         </ul>
+        <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted">Supprimer les tâches finies</button>
       </footer>
   </section>
 </template>
@@ -33,6 +36,8 @@ export default {
       todos: [],
       newTodo: '',
       filter: 'all',
+      editing: null,
+      oldTodo: '',
     };
   },
   methods: {
@@ -43,20 +48,42 @@ export default {
       });
       this.newTodo = '';
     },
+    deleteTodo(todo) {
+      this.todos = this.todos.filter(i => i !== todo);
+    },
+    deleteCompleted(todo) {
+      this.todos = this.todos.filter(todo => !todo.completed);
+    },
+    editTodo(todo) {
+      this.editing = todo;
+      this.oldTodo = todo.name;
+    },
+    doneEdit() {
+      this.editing = null;
+    },
+    cancelEdit() {
+      this.editing.name = this.oldTodo;
+      this.doneEdit();
+    },
   },
   computed: {
     allDone: {
       get() {
-
+        return this.remaining === 0;
       },
+
       set(value) {
-        this.todos.forEach((todo) => {
-          var todo.completed = value;
+        this.todos.forEach(function (todo) {
+          todo.completed = value;
         });
       },
     },
+
     remaining() {
       return this.todos.filter(todo => !todo.completed).length;
+    },
+    completed() {
+      return this.todos.filter(todo => todo.completed).length;
     },
     filteredTodos() {
       if (this.filter === 'todo') {
@@ -65,6 +92,16 @@ export default {
         return this.todos.filter(todo => todo.completed);
       }
       return this.todos;
+    },
+    hasTodos() {
+      return this.todos.length > 0;
+    },
+  },
+  directives: {
+    focus(el, value) {
+      if (value) {
+        el.focus();
+      }
     },
   },
 };
